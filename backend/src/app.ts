53253f353f7e345beb07b34c75cf3d8708be2aec
NguyenAndrew <NguyenAndrew@users.noreply.github.com>
2024-09-04 22:24:04 -0500

    Connect backend to frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,8 @@ const helmet = require("helmet");
 import host from "./host/host";
 import getConfig from "./config/config";
 import ConfigTemplate from "./config/configTemplate";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const PocketBase = require('pocketbase/cjs')
 
 const config: ConfigTemplate = getConfig();
 
@@ -17,6 +19,13 @@ app.use(helmet());
 app.get("/health", (req: Request, res: Response) => {
   axios.post(`${host}/axios-test`);
   res.status(200).send("Healthy!");
+});
+
+app.get("/pocketbase-message", async (req: Request, res: Response) => {
+  const pb = new PocketBase('http://127.0.0.1:8090');
+  const record = await pb.collection('hello_collection').getOne('23m7tql9bli4qiz');
+  const message = record.message;
+  res.status(200).send(message);
 });
 
 app.post("/axios-test", (req: Request, res: Response) => {
